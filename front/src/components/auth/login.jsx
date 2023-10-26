d4380680f3d99e2938e5e7bee43522f780c18b1e
Max linux PC <max11052003@yandex.ru>
2023-10-27 00:04:12 +0300

    Test

--- front/src/components/auth/login.jsx
+++ front/src/components/auth/login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 	const {setIsAuth } = useAuth()
-
+ 
 	const navigate = useNavigate()
 
 	const successLogin = (token, userIdd) => {
